fe9473d029dd52b0119acebae8987be005579e72
Cherie Tabb <cherietabb@gmail.com>
2019-02-11 18:41:31 -0600

    updated dependencies that were causing possible security vulnerabilities

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
-import App from './components/app';
 
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-ReactDOM.render(
+render(
 	<AppContainer>
 		<Provider store={createStoreWithMiddleware(reducers)}>
 			<BrowserRouter>
